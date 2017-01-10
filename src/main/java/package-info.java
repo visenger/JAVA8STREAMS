2ef8ed8d97e9a07e4f8730ce498385d593533694
visenger <visenger@gmail.com>
2017-01-10 11:46:47 +0100

    general info added;

---
+++ src/main/java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by visenger on 10/01/17.
+ *
+ * 1. all examples from de.berlin.beuth are taken from the book Java 8 in Action;
+ *
+ * 2. the benchmark example from de.berlin.benchmark is based on Java 8 MOOC from Oracle:
+ * https://github.com/visenger/jdk8-lambdas-streams-mooc-2016/tree/master/lesson3/homework/src
+ */
